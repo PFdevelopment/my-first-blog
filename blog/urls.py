cd1d6e5827a09528f4f80ee3cfc86ef1e03ce804
PFdevelopment <zpfdevelopment@gmail.com>
2017-10-02 12:00:02 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # r'^$' --> http://127.0.0.1:8000/
     # views.post_list --> view
     #
-    url(r'^$', views.post_list, name='post_list')
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
